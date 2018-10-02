a3626b5fb1203251a388d1aed293a911d03b770a
Daniel Rios <daniel.rios@jonasundderwolf.de>
2018-10-02 12:56:38 +0200

    overwrite inherited codes with scans

--- keyboards/nyquist/keymaps/misterrios/keymap.c
+++ keyboards/nyquist/keymaps/misterrios/keymap.c
@@ -102,11 +102,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
-  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSHIFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_ENT,  KC_ENT,  KC_SPC,  KC_SPC,  RAISE,   GUI,     KC_DEL , KC_BSPC \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSPC, \
+  _______, _______, _______, KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
+  _______, _______, KC_S,    KC_D,    KC_F,    KC_G,    _______, KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  _______, _______, _______, _______, _______, _______, KC_N,    _______, _______, _______, _______, KC_ENT , \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
 /* Lower
@@ -139,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, KC_DOWN, KC_RGHT, DE_QUES, KC_P7,   KC_P8,   KC_P9, \
   _______, _______, _______, _______, _______, _______, KC_LEFT, DE_AT,   DE_EURO, KC_P4,   KC_P5,   KC_P6, \
   _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, KC_P1,   KC_P2,   KC_P3, \
-  KC_LCTL, _______, _______, _______, KC_DEL,  KC_DEL,  KC_BSPC, KC_BSPC, _______, KC_P0,   KC_PDOT, KC_PENT \
+  _______, _______, _______, _______, KC_DEL,  KC_DEL,  KC_BSPC, KC_BSPC, _______, KC_P0,   KC_PDOT, KC_PENT \
 ),
 
 /* Raise
